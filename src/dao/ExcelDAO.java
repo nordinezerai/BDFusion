7968385d1432964372ed04c8ff80a110eb3996f8
nordinezerai <nordine.zerai@gmail.com>
2018-12-14 20:29:07 +0100

    commit nz

--- src/dao/ExcelDAO.java
+++ src/dao/ExcelDAO.java
@@ -2,13 +2,13 @@ package dao;
 
 import model.Enseignant;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExcelDAO {
+
+    String filepath = "resources\\Source1.xls";
 
     public ExcelDAO(){
 
@@ -24,9 +24,21 @@ public class ExcelDAO {
         List<Enseignant> listEns = new ArrayList<Enseignant>();
         try {
             Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-            String myDB = "jdbc:odbc:Driver={Microsoft Excel Driver (*.xls)};DBQ=resources/Source1.xls;" + "DriverID=22;READONLY=false";
+            String myDB = "jdbc:odbc:Driver={Microsoft Excel Driver (*.xls, *.xlsx, *.xlsm, *.xlsb)};DBQ="+ filepath +";" + "DriverID=22;READONLY=false";
 
             Connection con = DriverManager.getConnection(myDB, "", "");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select ID, Nom, Prenom from [2006$]");
+
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String nom = rs.getString(2);
+                String prenom = rs.getString(3);
+
+                System.out.println(id + " " + nom + " " + prenom);
+            }
+            rs.close();
+            stmt.close();
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
